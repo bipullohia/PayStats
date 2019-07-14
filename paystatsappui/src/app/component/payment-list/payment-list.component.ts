e720f7bf11845890dbaba3cea64b1f5eb956ad0f
bipullohia <bipul.lohia@gmail.com>
2019-07-15 00:41:35 +0530

    UI done for Manage Filters
    UI re-code for the Manage Filters to remove the bugs and make the code more staraight-forward

--- paystatsappui/src/app/component/payment-list/payment-list.component.ts
+++ paystatsappui/src/app/component/payment-list/payment-list.component.ts
@@ -11,54 +11,63 @@ import { ManageService } from 'src/app/service/manage.service';
   templateUrl: './payment-list.component.html',
   styleUrls: ['./payment-list.component.css']
 })
+
 export class PaymentListComponent implements OnInit {
-
-  payments: Payment[];
-  paymentEntities: PaymentEntity[];
-  paymentCategories: PaymentCategory[];
-  paymentModes: PaymentMode[];
-
-  isPresent: boolean = true;
-  // filter: Filter;
-
-  nullValueErrorBoolean: boolean = false;
-  errorMsg: string;
-
-  filterTypeOptions: string[];
-
-  paymentSelected: Payment;
-  indexSelected: number;
-
-  isEditingFilter: boolean = false;
-
   editLogo: string;
   garbageLogo: string;
   detailLogo: string;
   gridViewLogo: string;
   listViewLogo: string;
 
+  payments: Payment[];
+  paymentEntities: PaymentEntity[];
+  paymentCategories: PaymentCategory[];
+  paymentModes: PaymentMode[];
+  paymentSelected: Payment;
+  indexSelected: number;
+
+  //this is when no payment entity/cateogry/mode is selected in checkboxes 
+  nullValueErrorBoolean: boolean = false;
+  errorMsg: string;
+
+  //to store all the possible types of filters
+  allFilterTypeOptions: string[] = ["paymentDate", "paymentAmount",
+    "paymentCategory", "paymentMode", "paymentEntity"];
+
+  //to store all type of filters available at any given time
+  currentFilterTypes: string[] = [];
+
+  //to store all the filters which have been selected but may/maynot be applied yet
   filtersSelected: Filter[] = [];
+
+  //to store the filters which have been applied in the result
   filtersApplied: Filter[] = [];
 
+  //to store what type of filter is being dealt with currently
+  filterTypeSelected: string;
+
+  //to check if all the types of filters have already been selected
+  allFiltersSelectedBoolean: boolean = false;
+
+  //to check if the filter is being edited
+  isEditingFilter: boolean = false;
+
+
+  isGridView: boolean;
   sortingApplied: string;
-  filterTypeString: string;
-  filterNameString: string;
 
-  modalRef: BsModalRef;
-  detailsModalRef: BsModalRef;
-  filtersModalRef: BsModalRef;
-  addEditFilterModalRef: BsModalRef;
-
+  //these variables help to save default values in add/edit filter
   bsDateValue = new Date();
   bsDateRangeValue: Date[];
-  maxDate = new Date();
-
-allFiltersSelectedBoolean: boolean = false;
-
+  currentDate = new Date();
   minAmountFilter: number;
   maxAmountFilter: number;
 
-  isGridView: boolean;
+  modalRef: BsModalRef;
+  detailsModalRef: BsModalRef;
+  manageFiltersModalRef: BsModalRef;
+  addEditFilterModalRef: BsModalRef;
+
   constructor(private paymentService: PaymentService,
     private manageService: ManageService,
     private modalService: BsModalService,
@@ -71,45 +80,15 @@ allFiltersSelectedBoolean: boolean = false;
     this.listViewLogo = "assets/images/list-view.png";
   }
 
-
   ngOnInit() {
-
-    this.setAllValuesOfFilterType();
-
-    for (let option of this.filterTypeOptions) {
-      console.log(option);
-    }
     this.isGridView = false;
     this.getAllPayments();
     this.getAllPaymentCategory();
     this.getAllPaymentEntity();
     this.getAllPaymentModes();
-
-    
-    this.setDefaultValuesForFilter();
   }
 
-  setAllValuesOfFilterType() {
-    this.filterTypeOptions = ["paymentDate", "paymentAmount",
-      "paymentCategory", "paymentMode", "paymentEntity"];
-  }
-
-  setDefaultValuesForFilter() {
-    this.bsDateValue.setDate(this.maxDate.getDate() - 7);
-    //here maxDate is the current Date by default
-    this.bsDateRangeValue = [this.bsDateValue, this.maxDate];
-    this.minAmountFilter = 0;
-    this.maxAmountFilter = 500;
-    this.checkIfAllFiltersSelected();
-    this.filterTypeString = "";
-    this.filterNameString = "";
-    this.unselectAllItemsInTheArray(this.paymentEntities);
-    this.unselectAllItemsInTheArray(this.paymentCategories);
-    this.unselectAllItemsInTheArray(this.paymentModes);
-    this.nullValueErrorBoolean = false;
-    this.errorMsg = "";
-  }
-
+  //change the view from grid to list and vice-versa
   changeView(viewType: string) {
     if (viewType == "list") {
       this.isGridView = false;
@@ -145,33 +124,6 @@ allFiltersSelectedBoolean: boolean = false;
     );
   }
 
-  //this.isEditing = false;
-
-  // this.activatedRoute.params.subscribe(
-  //   (params) => {
-  //     let category = params['category'];
-  //     if (category) {
-  //       console.log("Category Tasks available");
-  //       this.paymentService.searchTasks("taskCategory", category).subscribe(
-  //         (data) => {
-  //           this.tasks = data;
-  //           //console.log(this.tasks + "activated");
-  //           this.isEditing = true;
-  //         }
-  //       );
-  //     } else {
-  //       console.log("All Tasks");
-  //       this.taskService.getAllTasks().subscribe(
-  //         (data) => {
-  //           this.tasks = data;
-  //           //console.log(this.tasks + "all tasks");
-  //         }
-  //       );
-  //     }
-  //   }
-  // );
-
-
   getAllPayments() {
     this.paymentService.getAllPayments().subscribe(
       (data) => {
@@ -182,11 +134,8 @@ allFiltersSelectedBoolean: boolean = false;
     );
   }
 
-
   sortArray(sortType: string) {
-
     switch (sortType) {
-
       case "mostRecentPaymentFirst":
         this.sortingApplied = "Most Recent Payment First";
         this.payments = this.payments.sort((n1, n2) => new Date(n2.dateOfTransaction).getTime() - new Date(n1.dateOfTransaction).getTime());
@@ -228,28 +177,98 @@ allFiltersSelectedBoolean: boolean = false;
     this.indexSelected = index;
   }
 
-  viewManageFiltersModal(template: TemplateRef<any>) {
-    // this.filterTypeString = "";
+  deletePayment() {
+    this.paymentService.deletePaymentById(this.paymentSelected.payid).subscribe(
+      (resp) => {
+        if (resp.ok) {
+          this.payments.splice(this.indexSelected, 1);
+          this.ngxNotificationService.sendMessage('Payment Record successfully deleted!', 'success', 'bottom-right');
+        } else {
+          this.ngxNotificationService.sendMessage('Payment Record deletion failed!', 'danger', 'bottom-right')
+        }
+      }
+    );
+  }
+
+  //set all the available filter type values to 'currentFilterTypes'
+  setAllValuesTocurrentFilterTypes() {
+    for (let i = 0; i < this.allFilterTypeOptions.length; i++) {
+      this.currentFilterTypes.push(this.allFilterTypeOptions[i]);
+    }
+  }
+
+  //converts the 'filter type' like 'filter name' (eg., paymentDate to Payment Date)
+  convertTypeToName(filterType: string): string {
+    let filterName = filterType.replace(/([A-Z])/g, ' $1').trim();
+    filterName = filterName[0].toUpperCase() + filterName.substring(1);
+    return filterName;
+  }
+
+  //to get a given date in proper string format
+  getDateString(date: Date): string {
+    let day = (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
+    let month = (date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1));
+    let dateString = day + "-" + month + "-" + date.getFullYear();
+    return dateString;
+  }
+
+  unselectAllItemsInTheArray(array: any[]) {
+    for (let item of array) {
+      item.selected = false;
+    }
+  }
+
+  setDefaultValuesForFilter() {
+    this.bsDateValue.setDate(this.currentDate.getDate() - 7);
+    this.bsDateRangeValue = [this.bsDateValue, this.currentDate];
+    this.minAmountFilter = 0;
+    this.maxAmountFilter = 500;
+    this.unselectAllItemsInTheArray(this.paymentEntities);
+    this.unselectAllItemsInTheArray(this.paymentCategories);
+    this.unselectAllItemsInTheArray(this.paymentModes);
+    this.filterTypeSelected = "";
+    this.nullValueErrorBoolean = false;
+    this.errorMsg = "";
+    this.isEditingFilter = false;
+  }
+
+  closeManageFiltersModal() {
+    this.manageFiltersModalRef.hide();
     this.filtersSelected = [];
-    this.setAllValuesOfFilterType();
-    this.checkIfAllFiltersSelected();
-    //this.filtersSelected = this.filtersApplied;
+  }
+
+  closeAddEditFilterModal() {
+    this.addEditFilterModalRef.hide();
     this.setDefaultValuesForFilter();
+  }
+
+  removeValueFromArray(value: string, array: string[]): string[] {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == value) {
+        array.splice(i, 1);
+      }
+    }
+    return array;
+  }
+
+  viewManageFiltersModal(template: TemplateRef<any>) {
+    this.filtersSelected = [];
     for (let filter of this.filtersApplied) {
       this.filtersSelected.push(filter);
     }
-    // this.checkIfAllFiltersSelected();
-
-    this.filtersModalRef = this.modalService.show(template, { class: 'modal-lg' });
+    this.checkIfAllFiltersSelected();
+    this.manageFiltersModalRef = this.modalService.show(template, { class: 'modal-lg', ignoreBackdropClick: true });
   }
 
   viewAddEditFilter(template: TemplateRef<any>, index: number) {
 
+    this.populateCurrentFilterTypeArray();
+    this.setDefaultValuesForFilter();
     if (index != null) {
       this.isEditingFilter = true;
-      this.filterTypeString = this.filtersSelected[index].filterType;
+      this.filterTypeSelected = this.filtersSelected[index].filterType;
 
-      if (this.filterTypeString == "paymentCategory") {
+      if (this.filterTypeSelected == "paymentCategory") {
         for (let value of this.filtersSelected[index].filterValues) {
           for (let category of this.paymentCategories) {
             if (value == category.categoryName)
@@ -258,7 +277,7 @@ allFiltersSelectedBoolean: boolean = false;
         }
       }
 
-      if (this.filterTypeString == "paymentEntity") {
+      if (this.filterTypeSelected == "paymentEntity") {
         for (let value of this.filtersSelected[index].filterValues) {
           for (let entity of this.paymentEntities) {
             if (value == entity.entityName)
@@ -267,7 +286,7 @@ allFiltersSelectedBoolean: boolean = false;
         }
       }
 
-      if (this.filterTypeString == "paymentMode") {
+      if (this.filterTypeSelected == "paymentMode") {
         for (let value of this.filtersSelected[index].filterValues) {
           for (let mode of this.paymentModes) {
             if (value == mode.paymodeName)
@@ -275,34 +294,67 @@ allFiltersSelectedBoolean: boolean = false;
           }
         }
       }
-
     }
-    
-    this.checkAllFilterTypeRepetitions();
-    this.addEditFilterModalRef = this.modalService.show(template, { class: 'modal-lg' });
+    this.addEditFilterModalRef = this.modalService.show(template, { class: 'modal-lg', ignoreBackdropClick: true });
   }
 
-
-
   removeFilter(index: number) {
-    this.filterTypeOptions.push(this.filtersSelected[index].filterType);
+    this.currentFilterTypes.push(this.filtersSelected[index].filterType);
     this.filtersSelected.splice(index, 1);
+    this.allFiltersSelectedBoolean = false;
+  }
+
+  //to populate the filter options in currentFilterTypes array (and remove the ones already selected)
+  populateCurrentFilterTypeArray() {
+    this.currentFilterTypes = [];
+    this.setAllValuesTocurrentFilterTypes();
+    if (this.filtersSelected.length != 0) {
+      for (let i = 0; i < this.filtersSelected.length; i++) {
+        this.currentFilterTypes = this.removeValueFromArray(this.filtersSelected[i].filterType, this.currentFilterTypes);
+      }
+    }
+  }
+
+  checkIfAllFiltersSelected() {
+    if (this.filtersSelected.length == this.allFilterTypeOptions.length) {
+      this.allFiltersSelectedBoolean = true;
+    } else {
+      this.allFiltersSelectedBoolean = false;
+    }
+  }
+
+  resetFilters() {
+    this.filtersSelected = [];
+    for (let filter of this.filtersApplied) {
+      this.filtersSelected.push(filter);
+    }
+    this.populateCurrentFilterTypeArray();
     this.checkIfAllFiltersSelected();
-    // console.log(filter + "(((");
-    // let i=0;
-    // for(let filter of this.filtersSelected){
-    //   if(filter == filter){
-    //     console.log("**" + filter.filterType);
-    //     this.filtersSelected.splice(i,1);
-    //   }
-    //   i++;
-    // }
+  }
+
+  applyFilters() {
+    if (this.filtersApplied.length == 0) {
+      this.filtersApplied = this.filtersSelected;
+    } else {
+      this.filtersApplied = [];
+      for (let filter of this.filtersSelected) {
+        this.filtersApplied.push(filter);
+      }
+    }
+    this.closeManageFiltersModal();
   }
 
   addFilter(filterType: string, filterValue: string[]) {
     let filter = new Filter();
     let filterValueArray: string[] = [];
-    this.isEditingFilter = false;
+
+    if (this.isEditingFilter) {
+      for (let i = 0; i < this.filtersSelected.length; i++) {
+        if (filterType == this.filtersSelected[i].filterType) {
+          this.filtersSelected.splice(i, 1);
+        }
+      }
+    }
 
     switch (filterType) {
       case "paymentDate":
@@ -335,135 +387,23 @@ allFiltersSelectedBoolean: boolean = false;
 
     if (filterValueArray.length == 0) {
       this.nullValueErrorBoolean = true;
-      this.errorMsg = "Error! Select atleast one " + this.convertTypeToName(this.filterTypeString);
+      this.errorMsg = "Error! Select atleast one " + this.convertTypeToName(this.filterTypeSelected);
     } else {
       filter.filterType = filterType;
       filter.filterName = this.convertTypeToName(filterType);
       filter.filterValues = filterValueArray;
 
-      // to not have filters of the same type added again
-      for (let i = 0; i < this.filtersSelected.length; i++) {
-        if (this.filtersSelected[i].filterType == filterType) {
-          this.filtersSelected.splice(i, 1);
+      // to remove the selected filter from filter options
+      for (let i = 0; i < this.currentFilterTypes.length; i++) {
+        if (this.currentFilterTypes[i] == filterType) {
+          this.currentFilterTypes.splice(i, 1);
           break;
         }
       }
 
       this.filtersSelected.push(filter);
-
-      this.checkFilterTypeRepetition(filterType);
-
-      // this.filterTypeString = "";
-      // this.filterNameString = "";
-      // this.unselectAllItemsInTheArray(this.paymentEntities);
-      // this.unselectAllItemsInTheArray(this.paymentCategories);
-      // this.unselectAllItemsInTheArray(this.paymentModes);
-      // this.nullValueErrorBoolean = false;
-      // this.errorMsg = "";
-      this.setDefaultValuesForFilter();
-      this.addEditFilterModalRef.hide();
+      this.checkIfAllFiltersSelected();
+      this.closeAddEditFilterModal();
     }
-
-
-  }
-
-  convertTypeToName(filterType: string): string {
-    let filterName = filterType.replace(/([A-Z])/g, ' $1').trim();
-    filterName = filterName[0].toUpperCase() + filterName.substring(1);
-    return filterName;
-  }
-
-  checkAllFilterTypeRepetitions(){
-    for(let option of this.filterTypeOptions){
-      this.checkFilterTypeRepetition(option);
-    }
-  }
-
-  checkFilterTypeRepetition(filterType: string) {
-    for (let filter of this.filtersSelected) {
-      if (filter.filterType == filterType) {
-        this.removeFilterTypeOptionsValue(filterType);
-      }
-    }
-    
-  }
-
-  checkIfAllFiltersSelected(){
-    if(this.filterTypeOptions.length == 0){
-      this.allFiltersSelectedBoolean = true;
-    }else{
-      this.allFiltersSelectedBoolean = false;
-    }
-  }
-
-  removeFilterTypeOptionsValue(value: string) {
-    for (let i = 0; i < this.filterTypeOptions.length; i++) {
-      if (this.filterTypeOptions[i] == value) {
-        this.filterTypeOptions.splice(i, 1);
-      }
-    }
-  }
-
-  resetFilters() {
-    this.filtersSelected = [];
-    for (let filter of this.filtersApplied) {
-      this.filtersSelected.push(filter);
-    }
-    this.checkIfAllFiltersSelected();
-  }
-
-  closeFilterModal() {
-    this.filtersModalRef.hide();
-    this.filtersSelected = [];
-    //this.setDefaultValuesForFilter();
-  }
-
-  getDateString(date: Date): string {
-    //let date = new Date();
-    let day = (date.getDate() < 10 ? "0" + date.getDate() : date.getDate());
-    let month = (date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1));
-    let dateString = day + "-" + month + "-" + date.getFullYear();
-    return dateString;
-  }
-
-  applyFilters() {
-    if (this.filtersApplied.length == 0) {
-      this.filtersApplied = this.filtersSelected;
-    } else {
-      this.filtersApplied = [];
-      for (let filter of this.filtersSelected) {
-        this.filtersApplied.push(filter);
-      }
-    }
-    this.closeFilterModal();
-  }
-
-  unselectAllItemsInTheArray(array: any[]) {
-    for (let item of array) {
-      item.selected = false;
-    }
-  }
-
-  closeAddEditFilterModal() {
-    this.addEditFilterModalRef.hide();
-    // this.unselectAllItemsInTheArray(this.paymentEntities);
-    // this.unselectAllItemsInTheArray(this.paymentCategories);
-    // this.unselectAllItemsInTheArray(this.paymentModes);
-    // this.filterTypeString = "";
-    // this.filterNameString = "";
-    this.setDefaultValuesForFilter();
-  }
-
-  deletePayment() {
-    this.paymentService.deletePaymentById(this.paymentSelected.payid).subscribe(
-      (resp) => {
-        if (resp.ok) {
-          this.payments.splice(this.indexSelected, 1);
-          this.ngxNotificationService.sendMessage('Payment Record successfully deleted!', 'success', 'bottom-right');
-        } else {
-          this.ngxNotificationService.sendMessage('Payment Record deletion failed!', 'danger', 'bottom-right')
-        }
-      }
-    );
   }
 }
