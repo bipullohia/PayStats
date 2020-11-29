0d6fec82e22689119dfae5ca323adf8cc71d28e1
Bipul Kumar Lohia <bipul.lohia@gmail.com>
2020-11-29 22:25:32 +0530

    UI enhancement - payment lists in ViewPayments
    UI enhancement - payment lists in ViewPayments

--- paystatsapp-ui/src/MainContainer.js
+++ paystatsapp-ui/src/MainContainer.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Container, Row, Col, Card, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import './MainContainer.css';
+import gridViewLogo from './assets/grid-view.png';
+import listViewLogo from './assets/list-view.png';
+import axios from 'axios';
+
+import { Container, Row, Col, Card, Navbar, Nav, NavDropdown, Button, DropdownButton, Dropdown, ButtonGroup, Image, ListGroup } from 'react-bootstrap';
+
+
 
 function MainContainer(){
+
     return(
         <div>
             <NavigationBar/>
             <SubContainer/>
         </div>
     );
-}
+};
 
 function NavigationBar(){
     return(
@@ -27,16 +35,100 @@ function NavigationBar(){
 }
 
 function SubContainer(){
+    
+    //All the state hooks
+    const [payments, setPayments] = useState([]);
+
+    //Effect hook to load all the payments
+    useEffect(() => {
+        axios.get(`http://localhost:7777/paymentss/all`).then((response) => {
+            setPayments(response.data);
+        });
+    }, []);
+
     return(
         <div>
-            <Container>
-                <Row>
-                    <Card>
-                        <Card.Body>This is some text within a card body.</Card.Body>
-                    </Card>
-                </Row>
+            <Container className="mt-2">
+               
+               <div className="mb-4 mt-4 border-bottom border-dark heading-border clearfix">
+                   <div className="float-left">
+                       <h4>View Payments</h4>
+                       <p>Showing {payments.length} results</p>
+                    </div>
+                   <div className="float-right mb-2">
+                        <ButtonGroup>
+                            <Button variant="info">Manage Filters</Button>
+                            <DropdownButton as={ButtonGroup} variant="outline-info" title="Sort By: " id="btn-grp-filter-sort">
+                                <Dropdown.Item href="#/action-1">Most Recent Payment First</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Oldest Payment First</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4">Payment Amount: Low to High</Dropdown.Item>
+                                <Dropdown.Item href="#/action-5">Payment Amount: High to Low</Dropdown.Item>
+                                <Dropdown.Item href="#/action-6">Recently Added Payment</Dropdown.Item>
+                        </DropdownButton>
+                        </ButtonGroup>
+
+                        <img src={gridViewLogo} className="img-icon mr-2 ml-3 img-thumbnail"/>
+                        <img src={listViewLogo} className="img-icon img-thumbnail"/>
+                    </div>
+               </div>
+               {printPaymentList(payments)}
             </Container>
         </div>
     );
+    
 }
+
+//to return all the payments in UI
+function printPaymentList(payments){
+    return (
+        <>
+            {/* <div>{payments.map( d => <div>{JSON.stringify(d)}</div>)}</div> */}
+            <ListGroup>
+                {payments.map(payment => {
+                    return (
+                    <ListGroup.Item>
+                        <span className="font-weight-bold mr-2">{payment.payDescription}</span>
+                        <span className="mr-2 ml-2">|</span>
+                        <span className="mr-2 ml-2">{payment.category}</span>
+                        <span className="mr-2 ml-2">|</span>
+                        <span className="mr-2 ml-2">₹{payment.amount}</span>
+                        <span className="mr-2 ml-2">|</span>
+                        <span className="mr-2 ml-2">{payment.payType}</span>
+                    <span className="mr-2 ml-2 small" style={{float: 'right'}}>Paid on {payment.transactionDate}</span>
+                    </ListGroup.Item>
+                    );
+                })}
+            </ListGroup>
+        </>
+    );
+}
+
+//simple post using Fetch
+function testFetchPost(){
+    fetch('http://localhost:7777/paymentss/', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            "payDescription": "testDesc",
+            "category": "Loan",
+            "amount": "1000",
+            "payType": "Debit",
+            "transactionDate": "13-03-2020"
+        })
+    })
+    .then(res => {
+        if(res.ok){
+            console.log("Response Received");
+            return res.json();
+            
+        }else{
+            console.log("ERROR occured");
+        }
+    })
+    .then(data => console.log(data));
+}
+
+
 export default MainContainer;
